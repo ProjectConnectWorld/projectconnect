e0b276dda1d31e9baea8b5cd418d198b384df3ca
ayanez17 <fredyyanez39@yahoo.com>
2017-07-17 11:31:44 -0400

    g

--- pages/getcountries.php
+++ pages/getcountries.php
@@ -1,15 +1,15 @@
 <?php
 
 function getAllCountries(){
-  echo "works";
-  // require "../../../db.php";
-  // $arr= array();
-  // $query = "SELECT DISTINCT(country_code) FROM schools ";
-  // $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
-  // while ($line = pg_fetch_object($result)) {
-  //    array_push($arr, $line->country_code);
-  // }
-  // return $arr;
+  // echo "works";
+  require "../../../db.php";
+  $arr= array();
+  $query = "SELECT DISTINCT(country_code) FROM schools ";
+  $result = pg_query($dbconn,$query) or die('Query failed: ' . pg_last_error());
+  while ($line = pg_fetch_object($result)) {
+     array_push($arr, $line->country_code);
+  }
+  return $arr;
 
 }
 
