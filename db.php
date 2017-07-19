abfde5d8bc53b68492e159600c3ec2d1e325f84d
ayanez17 <fredyyanez39@yahoo.com>
2017-07-19 12:08:22 -0400

    generic

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbconn = pg_connect("host=40.85.179.236 port=5432 dbname=project-connect_development user=fredy password=Sf8!4PuyY38nZ\$LwTuV9ssC")
-    or die('Could not connect: ' . pg_last_error());
-
-
-
- ?>
